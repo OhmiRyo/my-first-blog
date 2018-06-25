14c08deeb4131a1d7a678c648bd23d20f409b495
a5816015 <a5816015@aoyama.jp>
2018-06-25 17:34:07 +0900

    ファイル更新

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 's5$$1=)pg8u)p#$v74%_odfo5tw9*tze+jl&bl*j&crs66(kkg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
